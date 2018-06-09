20deac671b65e1a71e551888ff0fcae09a3fde7f
Kevin Chi <kevinchi118@gmail.com>
2018-06-09 18:09:11 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,15 +4,15 @@ class TicTacToe
   end
   
   WIN_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5], 
-  [6, 7, 8], 
-  [0, 3, 6], 
-  [1, 4, 7], 
-  [2, 5, 8], 
-  [0, 4, 8], 
-  [2, 4, 6]
-  ]
+    [0, 1, 2],
+    [3, 4, 5], 
+    [6, 7, 8], 
+    [0, 3, 6], 
+    [1, 4, 7], 
+    [2, 5, 8], 
+    [0, 4, 8], 
+    [2, 4, 6]
+    ]
  
   def current_player
     turn_count % 2 == 0 ? "X" : "O"
@@ -29,6 +29,9 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
+  
+  def input_to_index
+    
   
 
 end 
